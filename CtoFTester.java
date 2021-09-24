960c75cf39ffa4d10d4a5ceaadf418024476a6ae
alvinandboat <alvinboat123@gmail.com>
2021-09-23 21:56:17 -0400

    pls work

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,19 @@
+/*
+both methods should take in
+double parameters and return doubles.
+*/
+public class CtoFTester{
+  public static void main(String[] args){
+    System.out.println(celsiusToFahrenheit(33.0));
+    System.out.println(fahrenheitToCelsius(33.0));
+  }
 
+  public static double celsiusToFahrenheit(double x){
+    return (x * 9.0/5.0 + 32.0);
+  }
+
+  public static double fahrenheitToCelsius(double x){
+    return ((x - 32.0) * 5.0/9.0);
+  }
+
+}
